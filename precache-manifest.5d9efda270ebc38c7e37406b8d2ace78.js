8789ec0e7d7dabb9f1609b49841932d564af22e3
IsaacPeterson007 <brud.icp@gmail.com>
2019-10-10 20:00:08 -0500

    Updates

---
+++ precache-manifest.5d9efda270ebc38c7e37406b8d2ace78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be46456a40ff3d68512ff458b6a6ebba",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "48c9a546b72317af3898",
+    "url": "/portfolio/static/css/main.0841bc61.chunk.css"
+  },
+  {
+    "revision": "75ee71a79e3bd8de2140",
+    "url": "/portfolio/static/js/2.79287b79.chunk.js"
+  },
+  {
+    "revision": "48c9a546b72317af3898",
+    "url": "/portfolio/static/js/main.b9c109de.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.380dd9c45f37f3af91a713f241027ba3.js"
+  "/portfolio/precache-manifest.5d9efda270ebc38c7e37406b8d2ace78.js"
 );
 
 self.addEventListener('message', (event) => {
