6d69a984fb9ce018bc5975edf7f93e5a3ee64eb1
IsaacPeterson007 <brud.icp@gmail.com>
2019-10-16 23:36:09 -0500

    Updates

---
+++ precache-manifest.076da766988e3010f299d6c3b6daa9d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e832b49087ca73c882b032c4c8d330bd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b6ae26345b49efc43bf2",
+    "url": "/portfolio/static/css/main.856676a2.chunk.css"
+  },
+  {
+    "revision": "4828019ea1afcc37de84",
+    "url": "/portfolio/static/js/2.7c86d940.chunk.js"
+  },
+  {
+    "revision": "b6ae26345b49efc43bf2",
+    "url": "/portfolio/static/js/main.4acbac2b.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  },
+  {
+    "revision": "fc6159cbc5e2e5c23e450cd27dcc4ff7",
+    "url": "/portfolio/static/media/IsaacPeterson2019Resume.fc6159cb.pdf"
+  },
+  {
+    "revision": "87074ad542d6cebd1a5aee0ff72175c2",
+    "url": "/portfolio/static/media/Profile.87074ad5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6b7168d297afcccc3e994c116d2364a8.js"
+  "/portfolio/precache-manifest.076da766988e3010f299d6c3b6daa9d2.js"
 );
 
 self.addEventListener('message', (event) => {
