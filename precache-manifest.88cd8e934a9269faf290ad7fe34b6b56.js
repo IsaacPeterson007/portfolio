fc7161d2aba7df534907a5f0cc9fc977e97f8b19
IsaacPeterson007 <brud.icp@gmail.com>
2019-11-02 14:41:16 -0500

    Updates

--- precache-manifest.88cd8e934a9269faf290ad7fe34b6b56.js
+++ precache-manifest.88cd8e934a9269faf290ad7fe34b6b56.js
@@ -1,23 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8a35ac15d989157d17e69e2ea1c47c4d",
+    "revision": "6f08a0145ef3eeb9fb6563033d053f59",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "c33e4bae67bfb78c870e",
-    "url": "/portfolio/static/css/main.6180951d.chunk.css"
+    "revision": "2913ced1d61883e78cb1",
+    "url": "/portfolio/static/css/main.3c7b7a6c.chunk.css"
   },
   {
-    "revision": "25de3a4e4d6c7ef6eda3",
-    "url": "/portfolio/static/js/2.0c3b7a61.chunk.js"
+    "revision": "7a17f51f12ae729dbae2",
+    "url": "/portfolio/static/js/2.b163e6a9.chunk.js"
   },
   {
-    "revision": "c33e4bae67bfb78c870e",
-    "url": "/portfolio/static/js/main.0c3cddbe.chunk.js"
+    "revision": "2913ced1d61883e78cb1",
+    "url": "/portfolio/static/js/main.6ab496d0.chunk.js"
   },
   {
     "revision": "791f997dc0acbb20fb93",
     "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  },
+  {
+    "revision": "ce3b54317b0216ef5be6e36dd66f759d",
+    "url": "/portfolio/static/media/BwProfile.ce3b5431.jpg"
   },
   {
     "revision": "fc6159cbc5e2e5c23e450cd27dcc4ff7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.efc073a38673c91ba0a10241fe19eec4.js"
+  "/portfolio/precache-manifest.88cd8e934a9269faf290ad7fe34b6b56.js"
 );
 
 self.addEventListener('message', (event) => {
